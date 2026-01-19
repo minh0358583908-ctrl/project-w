5e52201a57e0cff7c6b9d881ab801aed6e2e3010
buiducminh <minh0358583908@gmail.com>
2026-01-19 10:44:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace baitapvenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int x = a++ + ++a + a++;
+            Console.WriteLine("Gia tri cua x=" + x);
+            Console.WriteLine("Gia tri cua a=" + a);
+            Console.ReadLine();
+        }
+    }
+}   
