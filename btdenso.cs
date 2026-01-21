7cb04fa723d7d74af44aa8baf8a6d6444b082ce8
buiducminh <minh0358583908@gmail.com>
2026-01-21 20:57:00 +0700

    Add files via upload

---
+++ btdenso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Các số nguyên tố nhỏ hơn 100 là:");
+
+        for (int i = 2; i < 100; i++)
+        {
+            bool isPrime = true;
+
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
