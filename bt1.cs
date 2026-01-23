fd8f4375c817da19d13dfc1e20c448266319be54
buiducminh <minh0358583908@gmail.com>
2026-01-23 20:04:34 +0700

    Add files via upload

---
+++ bt1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class LinearEquationResolver
+{
+    static void Main()
+    {
+        Console.Write("Nhập hệ số a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập hệ số b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        if (a != 0)
+        {
+            double x = -b / a;
+            Console.WriteLine("Phương trình có nghiệm duy nhất: x = " + x);
+        }
+        else
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phương trình có vô số nghiệm.");
+            }
+            else
+            {
+                Console.WriteLine("Phương trình vô nghiệm.");
+            }
+        }
+    }
+}
