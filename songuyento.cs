c0a2764b67022e6b9fe3e81a5f4aba68e39a3c24
buiducminh <minh0358583908@gmail.com>
2026-01-23 20:54:17 +0700

    Add files via upload

---
+++ songuyento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class songuyento
+{
+    static bool IsPrime(int n)
+    {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("nhap so: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(IsPrime(n) ? "la so nguyen to" : "khong phai so nguyen to");
+    }
+}
