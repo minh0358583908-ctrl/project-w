93023dd0eec0f27d61ca9fbe4b7c13029a6ed911
buiducminh <minh0358583908@gmail.com>
2026-01-23 20:00:08 +0700

    Add files via upload

---
+++ bt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class LoanInterestCalculator
+{
+    static void Main()
+    {
+        Console.Write("Nhập số tiền cho vay (VNĐ): ");
+        double soTienGui = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập lãi suất (%/năm): ");
+        double laiSuatNam = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập số tháng cho vay: ");
+        int soThang = int.Parse(Console.ReadLine());
+        double tienLai = soTienGui * (laiSuatNam / 100) / 12 * soThang;
+        double tongTien = soTienGui + tienLai;
+        Console.WriteLine("\n--- KẾT QUẢ ---");
+        Console.WriteLine("Tiền lãi: " + tienLai + " VNĐ");
+        Console.WriteLine("Tổng tiền nhận được: " + tongTien + " VNĐ");
+    }
+}
