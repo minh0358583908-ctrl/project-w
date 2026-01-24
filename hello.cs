6ab631e9591191949a1f903d72a76bd919939d23
buiducminh <minh0358583908@gmail.com>
2026-01-24 10:56:44 +0700

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class hello
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("bui duc minh: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello CodeGym: " + name);    
+    }
+}
