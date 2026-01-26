586187c0b4f2c3eddd21577dca658af7940820ca
buiducminh <minh0358583908@gmail.com>
2026-01-26 21:35:06 +0700

    Add files via upload

---
+++ giatrilonnhat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class giatrilonnhat
+{
+    static void Main()
+    {
+        int size;
+        int[] array;
+        do
+        {
+            Console.WriteLine("nhap kich thuoc mang:");
+            size = Int32.Parse(Console.ReadLine());
+            if (size > 20)
+                Console.WriteLine("kich thuoc khong vuot qua 20");
+        }
+        while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.Length)
+        {
+            Console.WriteLine("nhap phan tu thu" + (i + 1) + ":");
+            array[i] = Int32.Parse(Console.ReadLine()) ;
+            i++;
+
+        }
+        Console.WriteLine("danh sach tai san:");
+        for (int n = 0; n < array.Length; n++)
+        {
+            Console.WriteLine(array[n] + "\t");
+
+        }
+        int max = array[0];
+        int index = 1;
+        for (int n = 0; n < array.Length; ++n)
+        {
+            if (array[n] > max)
+            {
+                max = array[n];
+                index = n + 1;
+            }
+            Console.WriteLine("gia tri lon nhat trong danh sach la " + max + ",tai vi tri" +  index);
+                }
+
+    }
+}
---
+++ timgiatricuamang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class timgiatricuamang
+{
+    static void Main()
+    {
+        string[] students = {
+          "hoan",  "minh" , "hiep" , "hung", "hieu",
+          "hoa", "thao" , "tung" , "duong", "son"
+            };
+        Console.WriteLine("nhap ten sinh vien");
+        string input_name = Console.ReadLine();
+        bool isExist = false;
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].EndsWith(input_name))
+            {
+                Console.WriteLine("vi tri sinh vien co ten" + input_name + "la: " + (i + 1));
+                isExist = true;
+                break;
+            }
+            if (!isExist)
+            {
+                Console.WriteLine("khong tim thay sv" + input_name + "trong danh sach");
+            }
+        }
+    }
+}
