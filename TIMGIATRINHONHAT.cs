ab5d5bf2a624931451ff9314bc11aab97c1c8f05
buiducminh <minh0358583908@gmail.com>
2026-01-30 21:39:13 +0700

    Add files via upload

---
+++ TIMGIATRINHONHAT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    
+    public static int MinValue(int[] array)
+    {
+        int min = array[0];  
+        int index = 0;        
+
+     
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return index; 
+    }
+
+
+    public static void Main(string[] args)
+    {
+        int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+
+        int index = MinValue(arr);
+
+        Console.WriteLine("Gia tri nho nhat trong mang la: " + arr[index]);
+        Console.WriteLine("Vi tri cua no la index = " + index);
+    }
+}
---
+++ demkytu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+   
+    public static int CountCharacter(string str, char ch)
+    {
+        int count = 0; 
+
+       
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ch)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+    static void Main(string[] args)
+    {
+        string str = "lap trinh csharp";
+
+        Console.Write("Nhap ky tu can dem: ");
+        char ch = Console.ReadLine()[0];
+
+        int result = CountCharacter(str, ch);
+
+        Console.WriteLine("So lan xuat hien cua '" + ch + "' la: " + result);
+    }
+}
